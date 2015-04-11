30da8b08c32f60d983a13fb79949ca88d35280db
Ron Gruesbeck <rongruesbeck@gmail.com>
2015-04-10 20:07:03 -0700

    add new routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET landing page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  res.render('landing', { title: 'Napa Art Walk' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res) {
+  res.render('about', { title: 'Napa Art Walk | About' });
+});
+
+/* GET archive page. */
+router.get('/archive', function(req, res) {
+  res.render('archive', { title: 'Napa Art Walk | Archive' });
+});
+
+/* GET gallery page. */
+router.get('/gallery', function(req, res) {
+  res.render('gallery', { title: 'Napa Art Walk | Gallery' });
 });
 
 module.exports = router;
