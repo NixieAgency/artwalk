dd36479acb8ca41186e57226e62f3683f656d9f7
Ron Gruesbeck <rongruesbeck@gmail.com>
2015-05-06 21:03:09 -0700

    add artist service

--- client/app/views/artist/artist.js
+++ client/app/views/artist/artist.js
@@ -13,22 +13,37 @@ angular.module('myApp.artist', ['ngRoute'])
   });
 }])
 
+.service('Artist', ['$http', function($http){
+  this.query = function(cb){
+    $http.get("https://spreadsheets.google.com/feeds/list/" + "1fPfTlzipfy-dlZGOUFDW7n3T-ow8_TVUrrMDYyM2vTQ" + "/od6/public/values?alt=json")
+      .success(function(data){
+        cb(data.feed.entry.map(function(item){
+          console.log(item);
+          return {
+            title: item.gsx$title.$t,
+            artist: item.gsx$artist.$t
+          };
+        }));
+      });
+  };
+}])
+
 .controller('artistListCtrl', ['$scope', '$http', function($scope, $http) {
   $http.get('http://hipsterjesus.com/api/').success(function(data){
     $scope.ipsum = data.text;
   });
 
   console.log("hello from artist list");
-
 }])
-.controller('artistShowCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+.controller('artistShowCtrl', ['$scope', '$http', '$routeParams', 'Artist', function($scope, $http, $routeParams, Artist) {
 
-  console.log($routeParams);
+  Artist.query(function(artists){
+    console.log(artists);
+  });
+
+  console.log($routeParams.artistSlug);
 
   $http.get('http://hipsterjesus.com/api/').success(function(data){
     $scope.ipsum = data.text;
   });
-
-  console.log("hello from artist show");
-
 }]);
