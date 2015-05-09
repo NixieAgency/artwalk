6814785ab92ce7025956d2ff18c7d4ce9ac013dc
Ron Gruesbeck <rongruesbeck@gmail.com>
2015-05-08 22:08:52 -0700

    add get method to art service, and add artshow view

--- client/app/views/art/art.js
+++ client/app/views/art/art.js
@@ -15,13 +15,22 @@ angular.module('myApp.art', ['ngRoute'])
 
 .service('Art', ['$http', '$q', 'googleSheetsHelper', function($http, $q, googleSheetsHelper){
   var art_fields = ['platformnumber', 'title', 'nerrative', 'make', 'artist', 'hometown', 'email', 'website', 'image', 'audiolink', 'latitude', 'longitude'];
+  var url = "https://spreadsheets.google.com/feeds/list/1fPfTlzipfy-dlZGOUFDW7n3T-ow8_TVUrrMDYyM2vTQ/od6/public/values?alt=json";
+  //var url = "/client/components/artwalk.json";
 
   this.list = function(){
-    //var url = "https://spreadsheets.google.com/feeds/list/1fPfTlzipfy-dlZGOUFDW7n3T-ow8_TVUrrMDYyM2vTQ/od6/public/values?alt=json";
-    var url = "/client/components/artwalk.json";
     return $http.get(url)
       .then(function(res){
         return googleSheetsHelper.parse(art_fields, ['title', 'artist'], res.data);
+      });
+  };
+
+  this.get = function(slug){
+    return $http.get(url)
+      .then(function(res){
+        return googleSheetsHelper.parse(art_fields, ['title', 'artist'], res.data).filter(function(art){
+          return art.slug == slug;
+        })[0];
       });
   };
 }])
@@ -36,6 +45,8 @@ angular.module('myApp.art', ['ngRoute'])
 }])
 .controller('artShowCtrl', ['$scope', '$http', '$routeParams', 'Art', function($scope, $http, $routeParams, Art) {
 
-  console.log($routeParams.artSlug);
+  Art.get($routeParams.artSlug).then(function(art){
+    $scope.art = art;
+  });
 
 }]);
