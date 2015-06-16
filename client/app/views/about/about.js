73f1682d5662d234c9eb8448c6c809cbba2c4394
Ron Gruesbeck <rongruesbeck@gmail.com>
2015-06-15 17:25:06 -0700

    connect copy on about page

--- client/app/views/about/about.js
+++ client/app/views/about/about.js
@@ -9,35 +9,12 @@ angular.module('myApp.about', ['ngRoute'])
   });
 }])
 
-.service('mediaArchive', ['$http', '$q', 'googleSheetsHelper', function($http, $q, googleSheetsHelper){
-  var media_archive_url = googleSheetsHelper.jsonurl("1fPfTlzipfy-dlZGOUFDW7n3T-ow8_TVUrrMDYyM2vTQ", 5);
-  var media_archive_fields = [
-      'datepublished',
-      'publication',
-      'headline',
-      'author',
-      'link'
-  ];
+.controller('aboutCtrl', ['$scope', '$http', 'copycat', function($scope, $http, copycat) {
+  var copy = ['about', 'jury', 'vote', 'instagram-contest', 'audio-tour'];
 
-  this.list = function(){
-    return $http.get(media_archive_url)
-      .then(function(res){
-        return googleSheetsHelper.parse(media_archive_fields, ['headline', 'datepublished'], res.data).map(function(media){
-          media.date = Date.parse(media.datepublished);
-          return media;
-        }).filter(function(media){
-          return media.date;
-        });
-      });
-  };
-}])
-
-.controller('aboutCtrl', ['$scope', '$http', 'copycat', 'Lorem', 'mediaArchive', function($scope, $http, copycat, Lorem, mediaArchive) {
-  copycat.get('1-about').then(function(about){
-    $scope.about = about;
-  });
-
-  mediaArchive.list().then(function(media){
-    $scope.media = media;
+  copy.forEach(function(slug){
+    copycat.get(slug).then(function(body){
+      $scope[slug.replace('-', '')] = body;
+    });
   });
 }]);
