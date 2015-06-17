eadfbc95c3d171276ec4f868f1c0d0356e4186db
Ron Gruesbeck <rongruesbeck@gmail.com>
2015-06-17 15:56:59 -0400

    finnish sponsors page style and layout

--- client/app/views/landing/landing.js
+++ client/app/views/landing/landing.js
@@ -31,15 +31,7 @@ angular.module('myApp.landing', ['ngRoute'])
   };
 }])
 
-.value('Lorem', {
-  blurb: "Whatever art party Bushwick, cold-pressed church-key meditation fap vinyl fanny pack listicle. Locavore master cleanse McSweeney's aesthetic, occupy pug deep v asymmetrical Austin.",
-  short: '<p>Lorem ipsum dolor sit amet, et mel diam pertinacia. Id soleat populo mei, sit erat eruditi eu. Ut mel omnes diceret. Accusamus splendide in sea, in qui rebum bonorum voluptua. Ut fabulas nonumes eum, homero prompta conclusionemque at est. Eu cotidieque adversarium eam.</p><p>Ea graece dictas mollis sea. Te eos sapientem voluptaria, at accusamus efficiantur quo, sed cu enim ponderum tincidunt. Illud quaestio recteque ea his, pri at fabulas elaboraret. Amet zril per ea, usu alterum eligendi assentior an. Rebum facer ut per, ex tritani necessitatibus vel.</p>',
-  article: '<p>Lorem ipsum dolor sit amet, et mel diam pertinacia. Id soleat populo mei, sit erat eruditi eu. Ut mel omnes diceret. Accusamus splendide in sea, in qui rebum bonorum voluptua. Ut fabulas nonumes eum, homero prompta conclusionemque at est. Eu cotidieque adversarium eam.</p><p>Ea graece dictas mollis sea. Te eos sapientem voluptaria, at accusamus efficiantur quo, sed cu enim ponderum tincidunt. Illud quaestio recteque ea his, pri at fabulas elaboraret. Amet zril per ea, usu alterum eligendi assentior an. Rebum facer ut per, ex tritani necessitatibus vel.</p><p>Id vix harum ignota numquam, in his alii constituto sadipscing. Vel et pericula urbanitas, eam id dictas aperiam ancillae. Ut est omnes discere blandit, at quo ullum causae. Te mundi option prodesset pri, ad justo ornatus cum. In usu prima voluptua tractatos, nam tollit fabellas omittantur eu, usu te iriure sensibus. Ex unum semper principes est. Nonumy instructior pri et, etiam nostro accommodare ei pri.</p><p>Vulputate abhorreant vis ut, dolores facilisis duo ex. Sea te dolorum recteque consulatu, in utamur molestiae adipiscing nam. Ius id alterum voluptua, ei aliquam salutatus splendide vim, est id vide fuisset comprehensam. Per omnis sonet no, id pri bonorum accommodare, sea an qualisque reformidans.</p>'
-})
-
-.controller('landingCtrl', ['$scope', '$http', 'Lorem', 'Art', 'Program', 'News', function($scope, $http, Lorem, Art, Program, News) {
-
-  $scope.ipsumtheme = Lorem.article;
+.controller('landingCtrl', ['$scope', '$http', 'Art', 'Program', 'News', function($scope, $http, Art, Program, News) {
 
   News.list().then(function(news){
     $scope.recent_news = news;
